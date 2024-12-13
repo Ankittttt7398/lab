d21e4ff53e81aab10937b6667d976e3a253b905b
Ankittttt7398 <154671159+Ankittttt7398@users.noreply.github.com>
2024-12-13 11:56:46 +0530

    Add files via upload

---
+++ 1_3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+	int a,b;
+	printf("enter length and breadth of rectangle: ");
+	scanf("%d %d",&a,&b);
+	printf("Area of rectangle: %d",a*b);
+	return 0;
+}
---
+++ lab_1_1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+	int a,b,c;
+	printf("enter any two integer: ");
+	scanf("%d %d",&a,&b);
+	printf("sum: %d",a+b);
+	return 0;
+}
